b01afd2e7d57eaaa43e46e7fa2d9900c3105fcd7
Miquel Calafell <mcalafell@cifpfbmoll.eu>
2021-03-10 19:43:52 +0100

    feat(form): required

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -10,21 +10,21 @@
     <div class="form-group row">
         <label for="name" class="col-sm-2 col-form-label">Denominacion</label>
         <div class="col-sm-4">
-        <input name="name" type="text" class="form-control" id="input1" placeholder="Nombre" value="{{ old('Denominacion') }}>
+        <input name="name" type="text" class="form-control" id="input1" placeholder="Nombre" value="{{ old('Denominacion') }} required>
         </div>
     </div>
         
      <div class="form-group row">
         <label for="name" class="col-sm-2 col-form-label">Codigo</label>
         <div class="col-sm-4">
-        <input name="name" type="text" class="form-control" id="input2" placeholder="Nombre" value="{{ old('Codigo') }}>
+        <input name="name" type="text" class="form-control" id="input2" placeholder="Nombre" value="{{ old('Codigo') }} required>
         </div>
     </div>
         
     <div class="form-group row">
         <label for="CIF" class="col-sm-2 col-form-label">CIF</label>
         <div class="col-sm-4">
-        <input name="CIF" type="text" class="form-control" id="input3" placeholder="CIF" value="{{ old('CIF') }}>
+        <input name="CIF" type="text" class="form-control" id="input3" placeholder="CIF" value="{{ old('CIF') }} required>
         </div>
     </div>
         
@@ -40,21 +40,21 @@
         <div class="form-group row">
         <label for="Dir_postal" class="col-sm-2 col-form-label">Dir_postal</label>
         <div class="col-sm-4">
-        <input name="Dir_postal" type="text" class="form-control" id="input4" placeholder="Dir_postal" value="{{ old('Dir_postal') }}>
+        <input name="Dir_postal" type="text" class="form-control" id="input4" placeholder="Dir_postal" value="{{ old('Dir_postal') }} required>
         </div>
     </div>
         
     <div class="form-group row">
         <label for="CP" class="col-sm-2 col-form-label">CP</label>
         <div class="col-sm-4">
-        <input name="CP" type="text" class="form-control" id="input5" placeholder="CP" value="{{ old('CP') }}>
+        <input name="CP" type="text" class="form-control" id="input5" placeholder="CP" value="{{ old('CP') }} required>
         </div>
     </div>
 
         <div class="form-group row">
         <label for="Director_nom" class="col-sm-2 col-form-label">Director_nom</label>
         <div class="col-sm-4">
-        <input name="Director_nom" type="text" class="form-control" id="input6" placeholder="Director_nom" value="{{ old('Director_nom') }}>
+        <input name="Director_nom" type="text" class="form-control" id="input6" placeholder="Director_nom" value="{{ old('Director_nom') }} required>
         </div>
     </div>    
         
@@ -62,28 +62,28 @@
      <div class="form-group row">
         <label for="Direccior_apell" class="col-sm-2 col-form-label">Direcctor_apell</label>
         <div class="col-sm-4">
-        <input name="Direccior_apell" type="text" class="form-control" id="input7" placeholder="Primer Apellido" value="{{ old('Director_apell') }}>
+        <input name="Direccior_apell" type="text" class="form-control" id="input7" placeholder="Primer Apellido" value="{{ old('Director_apell') }} required>
         </div>
     </div>
         
     <div class="form-group row">
         <label for="Direcctor_apell2" class="col-sm-2 col-form-label">Direcctor_apell2</label>
         <div class="col-sm-4">
-        <input name="Direcctor_apell2" type="text" class="form-control" id="input8" placeholder="Direcctor_apell2" value="{{ old('Director_apell2') }}>
+        <input name="Direcctor_apell2" type="text" class="form-control" id="input8" placeholder="Direcctor_apell2" value="{{ old('Director_apell2') }} required>
         </div>
     </div>
         
     <div class="form-group row">
         <label for="Identificada" class="col-sm-2 col-form-label">Identificada</label>
         <div class="col-sm-4">
-        <input name="Identificada" type="text" class="form-control" id="input9" placeholder="Identificada" value="{{ old('Identificada') }}>
+        <input name="Identificada" type="text" class="form-control" id="input9" placeholder="Identificada" value="{{ old('Identificada') }} required>
         </div>
     </div>
         
     <div class="form-group row">
         <label for="Tipo_identificadal" class="col-sm-2 col-form-label">Tipo_identificada</label>
         <div class="col-sm-4">
-        <input name="Tipo_identificada" type="Tipo_identificada" class="form-control" id="input10" placeholder="Tipo_identificada" value="{{ old('Tipo_identificada') }}>
+        <input name="Tipo_identificada" type="Tipo_identificada" class="form-control" id="input10" placeholder="Tipo_identificada" value="{{ old('Tipo_identificada') }} required>
         </div>
     </div>
       
