6869260967083f452705f718b63401b5b2d8771d
Miquel Calafell <mcalafell@cifpfbmoll.eu>
2021-03-10 20:04:55 +0100

    feat(web.php):Crear las rutas del controlador

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DatosController;
+use App\Http\Controllers\CenDocenteController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'CenDocenteController@index')->name('homeIndex');
 
-Route::get('/', function () {
-	//return view('welcome');
-	return "Hola mundo";
-});
+Route::post('/', 'CenDocenteController@store')->name('homeStore');
 
-Route::resource('/datos',DatosController::class);
+Route::post('/', 'CenDocenteController@show')->name('homeShow');
+
+Route::post('/', 'CenDocenteController@edit')->name('homeEdit');
+
+Route::post('/', 'CenDocenteController@edit')->name('homeUpdate');
+
+Route::post('/', 'CenDocenteController@destroy')->name('homeDestroy');
+
+
+
+Route::resource('/datos',CenDocenteController::class);
